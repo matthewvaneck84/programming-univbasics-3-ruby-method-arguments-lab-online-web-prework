04ed3f68b0eb7ab764b332d333e19b949a1d89ea
Matthew Van Eck <thematt84@yahoo.com>
2019-07-17 06:11:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,4 +15,4 @@ def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 name = "Matt"
-language = "Python"
+
